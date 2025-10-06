5b0f24baec91cfaadaadf87e470c0821a90b206f
Vania Encinas Aduviri <127896956+VaniaEncinas@users.noreply.github.com>
2025-10-06 16:33:47 -0400

    Update pulsera_gps.sql

--- database/pulsera_gps.sql
+++ database/pulsera_gps.sql
@@ -1,17 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1:3307
--- Tiempo de generación: 06-10-2025 a las 22:06:30
--- Versión del servidor: 10.4.32-MariaDB
--- Versión de PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
