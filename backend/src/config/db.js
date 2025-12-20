5eb09f9eb292df69b1ca116d8e608394f4b2b21b
Vania <vaniaaduviri2001@gmail.com>
2025-12-19 21:34:31 -0400

    Primera versión del proyecto

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,14 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASS || "",
-  database: process.env.DB_NAME || "pulsera_gps",
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3307
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = pool;
